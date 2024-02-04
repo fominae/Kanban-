650654d396c026dd5b6e0fa7ee3d82d051979712
Your Name <your_email@whatever.com>
2024-02-05 01:33:28 +0700

    Создание и добавление карточки

---
+++ js/main.js
@@ -0,0 +1,31 @@
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            column1: [],
+            column2: [],
+            column3: [],
+            column4: [],
+            newCardTitle: '',
+            newCardDescription: '',
+            newCardDeadline: '',
+        }
+    },
+    methods: {
+        addCard() {
+            if (this.newCardTitle !== '') {
+                const newCard = {
+                    id: Date.now(),
+                    title: this.newCardTitle,
+                    description: this.newCardDescription,
+                    deadline: this.newCardDeadline,
+                    createdDate: new Date().toLocaleString(),
+                };
+                this.column1.push(newCard);
+                this.newCardTitle = '';
+                this.newCardDescription = '';
+                this.newCardDeadline = '';
+            }
+        },
+    }
+})
