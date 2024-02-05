10147f120fa8119012530644900c18543c74faaf
Your Name <your_email@whatever.com>
2024-02-05 14:27:47 +0700

    Дополнительное задание

--- js/main.js
+++ js/main.js
@@ -10,22 +10,99 @@ new Vue({
             newCardDescription: '',
             newCardDeadline: '',
             reasonForReturn: '',
+            newCardEmployee: '',
+            newCardPriority: '',
+            employees: [
+                { id: 1, name: 'Сотрудник 1' },
+                { id: 2, name: 'Сотрудник 2' },
+                { id: 3, name: 'Сотрудник 3' },
+            ],
         }
     },
+    computed: {
+        sortedColumn1() {
+            return this.column1.sort((a, b) => {
+                const priorityA = this.getPriorityLevel(a.priority);
+                const priorityB = this.getPriorityLevel(b.priority);
+                if (priorityA < priorityB) {
+                    return 1;
+                } else if (priorityA > priorityB) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
+        },
+        sortedColumn2() {
+            return this.column2.sort((a, b) => {
+                const priorityA = this.getPriorityLevel(a.priority);
+                const priorityB = this.getPriorityLevel(b.priority);
+                if (priorityA < priorityB) {
+                    return 1;
+                } else if (priorityA > priorityB) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
+        },
+        sortedColumn3() {
+            return this.column3.sort((a, b) => {
+                const priorityA = this.getPriorityLevel(a.priority);
+                const priorityB = this.getPriorityLevel(b.priority);
+                if (priorityA < priorityB) {
+                    return 1;
+                } else if (priorityA > priorityB) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
+        },
+        sortedColumn4() {
+            return this.column4.sort((a, b) => {
+                const priorityA = this.getPriorityLevel(a.priority);
+                const priorityB = this.getPriorityLevel(b.priority);
+                if (priorityA < priorityB) {
+                    return 1;
+                } else if (priorityA > priorityB) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
+        },
+    },
+
     methods: {
         addCard() {
-            if (this.newCardTitle !== '') {
+            if (this.newCardTitle !== ''&& this.newCardPriority !== '') {
                 const newCard = {
                     id: Date.now(),
                     title: this.newCardTitle,
                     description: this.newCardDescription,
                     deadline: this.newCardDeadline,
+                    employee: this.newCardEmployee,
                     createdDate: new Date().toLocaleString(),
+                    priority: this.newCardPriority,
                 };
                 this.column1.push(newCard);
                 this.newCardTitle = '';
                 this.newCardDescription = '';
                 this.newCardDeadline = '';
+                this.newCardEmployee = '';
+            }
+        },
+        getPriorityLevel(priority) {
+            switch (priority) {
+                case 'Низкий':
+                    return 1;
+                case 'Средний':
+                    return 2;
+                case 'Высокий':
+                    return 3;
+                default:
+                    return 0;
             }
         },
         deleteCard(card) {
